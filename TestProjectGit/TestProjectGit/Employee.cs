1b4fa372e6d1978825e2765f1dcaab434a5ff849
baibhavgh550 <baibhav.gh550@gmail.com>
2018-12-02 19:55:46 -0600

    Added a new class

---
+++ TestProjectGit/TestProjectGit/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectGit
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        IEnumerable<String> employees { get; set; }
+
+
+    }
+}
